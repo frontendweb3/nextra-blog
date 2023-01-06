42311d245274667df95b24a414c3776caaf2ee25
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-06 23:25:47 +0530

    set image unoptimized into true

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = withNextra({ 
+    images: {
+        unoptimized: true
+    }
+})

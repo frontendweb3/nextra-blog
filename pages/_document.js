403cf05dd596a024127072bd66710c732ab5377a
officialrajdeepsingh <radewala.2@gmail.com>
2023-01-06 22:10:14 +0530

    update SEO config for blog

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Nextra Blog",
+    title: "Demo blog create by Rajdeep singh",
     description: "Clone and deploy your own Next.js-powered portfolio in minutes.",
     image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
   };
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@Official_R_deep" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
